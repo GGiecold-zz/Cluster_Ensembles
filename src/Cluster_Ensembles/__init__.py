2aa1dd0aa0603f86167764c771eb19a3c57d4b32
Gregory Giecold <GGiecold@users.noreply.github.com>
2015-12-08 10:12:09 -0500

    Update __init__.py

--- src/Cluster_Ensembles/__init__.py
+++ src/Cluster_Ensembles/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 
-
-
 # Cluster_Ensembles/src/Cluster_Ensembles/__init__.py;
 
 # Author: Gregory Giecold for the GC Yuan Lab
@@ -10,7 +8,7 @@
 # Contact: g.giecold@gmail.com, ggiecold@jimmy.harvard.edu
 
 
-r"""Cluster_Ensembles is a package for combining multiple partitions 
+"""Cluster_Ensembles is a package for combining multiple partitions 
 into a consolidated clustering.
 The combinatorial optimization problem of obtaining such a consensus clustering
 is reformulated in terms of approximation algorithms for 
@@ -27,24 +25,9 @@ In: Journal of Machine Learning Research, 3, pp. 583-617. 2002
 """
 
 
-
-
-#*********************************************************************************
-#*********************************************************************************
-
-
 from . Cluster_Ensembles import *
-
-
-#*********************************************************************************
-#*********************************************************************************
 
 
 __all__ = []
 __all__ += Cluster_Ensembles.__all__
-
-
-#*********************************************************************************
-#*********************************************************************************
-
 
